8372d7b41fadb88703dafe294c5a0a5ba74ca9d3
Julia Barbosa <juliadealencarb@gmail.com>
2023-02-27 15:17:59 +0000

    Adding changes.

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const fruitForm = document.querySelector('#inputSection form');
 const fruitList = document.querySelector('#fruitSection ul');
+const fruitNutrition = document.querySelector('#nutritionSection p')
 
 fruitForm.addEventListener("submit", extractFruit);
+
+let calorieCount = 0;
 
 
 // fruitList.addEventListener('click', e => {
@@ -15,7 +18,8 @@ function extractFruit(e) {
     // console.log(e.target[0].value) // e = object created when the form is submitted, target can be used to access the value passe dby the user
     let fruitInput = e.target.fruitInput.value;
     if (fruitInput) {
-        addFruit(fruitInput);
+        // addFruit(fruitInput);
+        fetchFruitData(fruitInput)
     }
     // e.target.fruitInput.value = ''
     e.target.reset()
@@ -23,11 +27,13 @@ function extractFruit(e) {
 // }   console.log(e.target.fruitInput.value) it's supposed to do the same thing if the id didn't have a dash
 
 function addFruit(fruit) {
+    // console.log(fruit)
     // create list item
     const li = document.createElement('li');
 
     //asign text to list item
-    li.textContent = fruit;
+    li.textContent = fruit["name"];
+    getCalories(fruit)
     li.addEventListener('click', removeFruit, {once: true}); //runs the event listener only once
     // li.removeEventListener('click', removeFruit);
 
@@ -37,4 +43,26 @@ function addFruit(fruit) {
 
 function removeFruit(e) {
     e.target.remove();
+}
+
+function fetchFruitData(fruit) {
+    fetch(`https://fruity-api.onrender.com/fruits/${fruit}`)
+        .then(resp => processResponse(resp))
+        .then(data => addFruit(data))
+        .catch((e) => console.log(e))
+}
+
+function getCalories(fruitData) {
+    let fruitCalories = fruitData['nutritions']['calories']
+    calorieCount += fruitCalories
+    document.querySelector("p").innerHTML = `Calorie count: ${calorieCount}`
+}
+
+function processResponse(resp) {
+    if(resp.ok) {
+        return resp.json()
+    }
+    else {
+        throw `Error: HTTP status code = ${resp.status}.`
+    }
 }
